83cdf0caca8391a2b8ee4ab6b4872d75a9cf8c85
Sandeep-Balli-Rapidsoft <sandeepballi.rapidsoft@gmail.com>
2023-09-12 18:46:20 +0530

    new

--- Spring-Security/workspace/jwt/src/main/java/jwt/config/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package jwt.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
